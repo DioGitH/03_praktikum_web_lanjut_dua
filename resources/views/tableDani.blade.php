e675f006e49108f1c279370174202aaf423dfaf4
= <=>
2023-03-13 11:51:14 +0700

    update

--- resources/views/tableDani.blade.php
+++ resources/views/tableDani.blade.php
@@ -2,7 +2,42 @@
 
 @section('title', 'Dashboard')
 
+@section('content_header')
+<h1>Table Dani</h1>
+@stop
+
 @section('content')
+
+    {{-- Setup data for datatables --}}
+@php
+$heads = [
+    'ID',
+    'Nama',
+    ['Pengalaman' => 'Phone', 'width' => 40],
+];
+
+$config = [
+    'data' => [
+        [22, 'John Bender', '+02 (123) 123456789'],
+        [19, 'Sophia Clemens', '+99 (987) 987654321'],
+        [3, 'Peter Sousa', '+69 (555) 12367345243'],
+    ],
+    'order' => [[1, 'asc']],
+    'columns' => [null, null, null, ['orderable' => false]],
+];
+@endphp
+
+{{-- Minimal example / fill data using the component slot --}}
+<x-adminlte-datatable id="table1" :heads="$heads" head-theme="dark" :config="$config"
+    striped hoverable bordered compressed>
+    @foreach($config['data'] as $row)
+        <tr>
+            @foreach($row as $cell)
+                <td>{!! $cell !!}</td>
+            @endforeach
+        </tr>
+    @endforeach
+</x-adminlte-datatable>
 
 @stop
 
